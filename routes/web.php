b903288d27c4571ab6645914b3064fc82a0362e3
a-mamal <mamalikidoua@gmail.com>
2026-01-18 19:36:37 +0200

    feat(pages): move home to pages folder, add empty page placeholders, update routes
    - Created `resources/views/pages/` folder
    - Moved `home.blade.php` into `pages/`
    - Added empty Blade files for: education, languages, certificates, projects, experience
    - Updated `web.php` routes to point to the new pages
    - Prepared structure for multi-page Laravel portfolio

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', function() {
+    return view('pages.home');
+})->name('home');
+
+Route::get('/education', function() {
+    return view('pages.education');
+})->name('education');
+
+Route::get('/languages', function() {
+    return view('pages.languages');
+})->name('languages');
+
+Route::get('/certificates', function() {
+    return view('pages.certificates');
+})->name('certificates');
+
+Route::get('/projects', function() {
+    return view('pages.projects');
+})->name('projects');
+
+Route::get('/experience', function() {
+    return view('pages.experience');
+})->name('experience');
