b69b7c9cd1b2812d76fb9db0e38eb0da10de75cd
a-mamal <mamalikidoua@gmail.com>
2026-01-18 18:38:51 +0200

    feat(header): populate header partial with site title and nav
    - Adds site title and main navigation links to header
    - Placeholder anchors (#) for now; update to routes or page sections later

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <h1>Mamalikidou Anastasia</h1>
+    <p>BEng in Computer Science and Engineering - Software Specialisation</p>
+
+    <nav class="site-nav">
+        <a href="#">Education</a>
+        <a href="#">Languages</a>
+        <a href="#">Certificates</a>
+        <a href="#">Projects</a>
+        <a href="#">Work Experience</a>
+    </nav>
+</header>
