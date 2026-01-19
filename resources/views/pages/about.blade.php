e4109a831e71a758bed4ac51206361a843e99306
a-mamal <mamalikidoua@gmail.com>
2026-01-19 08:01:14 +0200

    chore(about): include all partials in About page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('partials.about.bio')
+    @include('partials.about.education')
+    @include('partials.about.experience')
+    @include('partials.about.certifications')
+    @include('partials.about.languages')
+@endsection
