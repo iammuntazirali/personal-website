45feed92174c6dee04d72c9756bab2228ab77485
a-mamal <mamalikidoua@gmail.com>
2026-01-21 12:15:21 +0200

    refactor(routes): merge custom pages with Breeze auth routes and remove old

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,25 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Public pages
+Route::get('/', function() {
+    return view('pages.home');
+})->name('home');
+
+Route::get('/projects', function() {
+    return view('pages.projects');
+})->name('projects');
+
+Route::get('/about', function() {
+    return view('pages.about');
+})->name('about');
+
+Route::get('/contact', function() {
+    return view('pages.contact');
+})->name('contact');
+
+
+// Authenticated routes (from Breeze)
 
 Route::get('/dashboard', function () {
     return view('dashboard');
