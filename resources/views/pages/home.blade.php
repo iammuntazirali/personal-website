b108f34ea556434b567f19457dcef584fc81973e
a-mamal <mamalikidoua@gmail.com>
2026-01-25 22:53:52 +0200

    refactor(home): remove second header

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,4 @@
 <x-app-layout :title="'Home | Mamalikidou Anastasia'">
-    <x-slot name="header">
-        <h1>Mamalikidou Anastasia</h1>
-    </x-slot>
 
     <h2>Welcome to my website!</h2>
     @include('partials.home.hero')
