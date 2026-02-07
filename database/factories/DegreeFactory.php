7b8fa3948e256bdfa0807d1caa0da71969b8754b
Anastasia Mamalikidou <mamalikidoua@gmail.com>
2026-02-07 10:53:15 +0200

    chore(seeders): add Degree factory and local seeder (#34)

--- database/factories/DegreeFactory.php
+++ database/factories/DegreeFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Degree;
+use App\Models\Profile;
+use App\Models\Issuer;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Degree>
@@ -17,7 +20,21 @@ class DegreeFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'profile_id' => Profile::factory(),
+            'issuer_id' => Issuer::factory(),
+
+            // Degree details
+            'title' => $this->faker->sentence(3),
+            'level' => $this->faker->randomElement(['Bachelor', 'Master', 'PhD']), // required
+            'field' => $this->faker->optional()->word(), 
+
+            // Dates
+            'start_date' => $this->faker->optional()->date('Y-m-d', '2020-01-01'),
+            'end_date' => $this->faker->optional()->date('Y-m-d', '2024-12-31'),
+
+            // Grade and image
+            'grade' => $this->faker->optional()->randomElement(['A+', 'A', 'B+', 'B', '3.8', '4.0']),
+            'image' => null
         ];
     }
 }
--- database/seeders/DegreeSeeder.php
+++ database/seeders/DegreeSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Degree;
+use App\Models\Profile;
+use App\Models\Issuer;
 
 class DegreeSeeder extends Seeder
 {
@@ -12,6 +15,16 @@ class DegreeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $profile = Profile::first(); // pick first profile
+        $issuers = Issuer::all();    // get all issuers
+
+        Degree::factory(3)->create([
+            'profile_id' => $profile->id,
+            
+            // pick a random issuer for each degree
+            'issuer_id' => function () use ($issuers) {
+                return $issuers->random()->id; 
+            },
+        ]);
     }
 }
